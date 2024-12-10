7b5209a71bc08896c7c6660f61e9330734273fd6
waseemtasawar <waseemtasawar97@gmail.com>
2024-12-10 17:47:40 +0500

    add directory

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express")
-
+const fs = require("fs")
 const app = express();
 
-app.get('/',(req, res)=>{
-  res.status(200).json({message:"Hello from server",
-    app:'Natours'
-  })
-})
-app.post('/',(req, res)=>{
-  res.status(200).send("you can write post at this endpoint")
-})
+// app.get('/',(req, res)=>{
+//   res.status(200).json({message:"Hello from server",
+//     app:'Natours'
+//   })
+// })
+// app.post('/',(req, res)=>{
+//   res.status(200).send("you can write post at this endpoint")
+// })
+
+const tours = fs.readFileSync(`${__dirname}/dev-data/data`)
+
 
 
 const port = 3000
