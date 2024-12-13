365419c4aed73f544635dafb0d8e924b6ba1435b
waseemtasawar <waseemtasawar97@gmail.com>
2024-12-13 16:25:38 +0500

    working with API's

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require("express")
 const fs = require("fs")
 const app = express();
-
-// app.get('/',(req, res)=>{
-//   res.status(200).json({message:"Hello from server",
-//     app:'Natours'
-//   })
-// })
+app.use(express.json())
+app.get('/',(req, res)=>{
+  res.status(200).json({message:"Hello from server",
+    app:'Natours'
+  })
+})
 // app.post('/',(req, res)=>{
 //   res.status(200).send("you can write post at this endpoint")
 // })
 
-const tours = fs.readFileSync(`${__dirname}/dev-data/data`)
+const tours = JSON.parse( fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours',(req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data:{
+            tours
+        }
+    })
+})
+
+app.post('/api/v1/tours',(req, res)=>{
+    console.log(req.body)
+    res.send("Done")
+})
 
 
 
