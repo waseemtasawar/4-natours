fe946d0d7e5b39c1bbb71de28efd32a0ba4a91c6
waseemtasawar <waseemtasawar97@gmail.com>
2024-12-17 16:20:39 +0500

    Connection of DataBase

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,14 +1,19 @@
 /*global require,  module, */
 const express = require('express')
+// import express from 'express'
 const tourController = require('./../controllers/tourController')
+
 const router = express.Router()
 
 
 router.param('id', tourController.checkId)
 
 
-    router.route('/').get(tourController.getAllTours).post(tourController.checkbody ,tourController.addTour)
-    router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour)
+router.route('/').get(tourController.getAllTours)
+.post(tourController.checkbody ,tourController.addTour)
 
+router.route('/:id').get(tourController.getTour)
+.patch(tourController.updateTour)
+.delete(tourController.deleteTour)
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-/*global require, process,*/
+/*global require,  process, */
 const dotenv = require('dotenv')
 const app = require('./app')
-
+const mongoose = require('mongoose')
 dotenv.config({path:'./congif.env'})
+
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+// const DB = process.env.DATABASE;
+mongoose.connect(DB).then(()=>{
+    console.log("DB connected Successfully")
+}).catch((error)=>{
+    console.log("DB is not Connected", error)
+})
+
+const tourSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: [true,"A tour have nust name"],
+        unique:true
+    },
+    rating : {
+        type: Number,
+        default:4.5 
+    },
+    price : {
+        type: Number,
+        required: [true,"A tour have nust price"],
+        
+    }
+})
+
+const Tour = mongoose.model('Tour', tourSchema)
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
