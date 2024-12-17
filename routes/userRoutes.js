cc7413125509f74a41c285b29d18e7f26be347c2
waseemtasawar <waseemtasawar97@gmail.com>
2024-12-17 17:04:17 +0500

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,13 @@ const userController = require('./../controllers/userController')
 const router = express.Router()
 
 
-router.route('/').get(userController.getAllUsers).post(userController.addUser)
-router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
+router.route('/')
+.get(userController.getAllUsers)
+.post(userController.addUser)
+router.route('/:id')
+.get(userController.getUser)
+.patch(userController.updateUser)
+.delete(userController.deleteUser)
 
 
 module.exports = router
