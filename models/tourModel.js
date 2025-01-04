0107551dd55f7ebfa437810a13cee7d8665823a7
Waseem Tasawar <waseemtasawar96@gmail.com>
2025-01-04 15:57:37 +0500

    Document middle, Query MiddleWare , Aggrigation middleWare

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 /*global require,  module*/
 const mongoose = require('mongoose');
-
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -9,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have durations'],
@@ -54,7 +55,12 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: true,
+    },
   },
+
   {
     toJSON: { virtual: true },
     toObject: { virtuals: true },
@@ -65,8 +71,38 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-tourSchema.pre('save', function () {
-  console.log(this);
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save Document');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+//  QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+tourSchema.post(/^find/, function (doc, next) {
+  console.log(`Query took ${Date.now - this.start} miliseconds!`);
+  // console.log(doc);
+  next();
+});
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
 });
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
