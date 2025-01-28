0247afc387577681e9bad7659d00ed01fd2493a3
Waseem Tasawar <waseemtasawar96@gmail.com>
2025-01-29 01:55:31 +0500

    Impliment Jwt and Security

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,42 @@
-/*global exports, */
+/*global exports, require */
 
-exports.getAllUsers = (req, res)=>{
-    res.status(500).json({
-        status: 'fail',
-        message:'route cannot define yet'
-    })
-}
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.addUser = (req, res)=>{
-    res.status(500).json({
-        status: 'fail',
-        message:'route cannot define yet'
-    })
-}
-exports.getUser = (req, res)=>{
-    res.status(500).json({
-        status: 'fail',
-        message:'route cannot define yet'
-    })
-}
-exports.updateUser = (req, res)=>{
-    res.status(500).json({
-        status: 'fail',
-        message:'route cannot define yet'
-    })
-}
-exports.deleteUser = (req, res)=>{
-    res.status(500).json({
-        status: 'fail',
-        message:'route cannot define yet'
-    })
-}
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  // Send Response
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
+exports.addUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'route cannot define yet',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'route cannot define yet',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'route cannot define yet',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'route cannot define yet',
+  });
+};
--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -37,6 +38,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassowrd,
+) {
+  return await bcrypt.compare(candidatePassword, userPassowrd);
+};
 
 const User = mongoose.model('User', userSchema);
 
