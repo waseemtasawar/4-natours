16bcc682d280af2df0cd962367395e8474fa7bbc
Waseem Tasawar <waseemtasawar96@gmail.com>
2025-05-07 22:28:37 +0500

    work in progress

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,12 @@
 /*global require, module exports */
-
+const Tour = require('../models/tourModel');
 exports.getOverview = (req, res) => {
+  // 1) get tour data from collection
+
+  // 2) Build a template
+
+  // 3) render that template using tour data from 1)
+
   res.status(200).render('overview', {
     title: 'All Tours',
   });
