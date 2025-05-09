b6f832516c8e95904ac83b847f846237fb1e932b
Waseem Tasawar <waseemtasawar96@gmail.com>
2025-05-10 02:44:05 +0500

    making tmeplate through pug (tour detail)

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,19 +1,30 @@
 /*global require, module exports */
 const Tour = require('../models/tourModel');
-exports.getOverview = (req, res) => {
-  // 1) get tour data from collection
+const catchAsync = require('../utils/catchAsync');
 
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) get tour data from collection
+  const tours = await Tour.find();
   // 2) Build a template
 
   // 3) render that template using tour data from 1)
 
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2) Build template
+  // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
+    tour,
   });
-};
+});
